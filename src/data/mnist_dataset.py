2517d200f5eabb8df7411445a6fd3f63ce55a266
Christian Mancini <christian.mancini@unifi.it>
2025-12-17 11:35:56 +0100

    added mnist datatset
    Signed-off-by: Christian Mancini <christian.mancini@unifi.it>

---
+++ src/data/mnist_dataset.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from typing import LiteralString
+
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+
+class MNISTDataModule:
+
+    def __init__(self, data_dir: str = "./datasets", batch_size: int = 32, num_workers: int = 4):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.data_train = None
+        self.data_val = None
+        self.data_test = None
+        self.num_classes = 10
+        self.img_shape = (1, 28, 28)
+
+    def prepare_data(self):
+        MNIST(self.data_dir, train=True, download=True)
+        MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            mnist_full = MNIST(self.data_dir, train=True, download=True, transform=self.transform)
+            self.data_train, self.data_val = random_split(
+                mnist_full, [11/12, 1/12]
+            )
+        if stage == "test":
+            self.data_test = MNIST(self.data_dir, train=False, download=True, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.data_train, batch_size=self.batch_size, drop_last=True, shuffle=True, pin_memory=True,
+                          num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.data_val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.data_test, batch_size=self.batch_size, num_workers=self.num_workers)
