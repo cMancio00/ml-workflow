64e410e8cbd10d27bcb81b0d7d03cd992cbcd1f0
Christian Mancini <christian.mancini@unifi.it>
2025-12-20 12:05:36 +0100

    Added parametrizations on loss monitoring
    Signed-off-by: Christian Mancini <christian.mancini@unifi.it>

--- src/trainer.py
+++ src/trainer.py
@@ -1,6 +1,5 @@
 import torch
 from lightning.pytorch import Trainer, seed_everything
-from lightning.pytorch.callbacks import RichProgressBar, EarlyStopping
 import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig
@@ -10,44 +9,53 @@ import optuna
 from optuna import Trial
 from optuna.integration.pytorch_lightning import PyTorchLightningPruningCallback
 
-def objective(trial: Trial, cfg: DictConfig):
-    seed_everything(42, workers=True)
 
-    torch.set_float32_matmul_precision('high')
-    lr = trial.suggest_float(
+def build_callbacks(cfg: DictConfig):
+    if "callbacks" not in cfg:
+        return None
+    return [instantiate(cb) for cb in cfg.callbacks.values()]
+
+def objective(trial: Trial, cfg: DictConfig):
+
+    cfg.optim.lr = trial.suggest_float(
         name='lr',
         low=1e-4,
         high=1e-1,
         log=True
     )
 
+
+    cfg.data.batch_size = trial.suggest_categorical('batch_size', [32, 64, 128])
+
+    return train(cfg)
+
+@hydra.main(version_base=None, config_path="pkg://config", config_name="trainer")
+def train(cfg: DictConfig):
+    seed_everything(42, workers=True)
+
+    torch.set_float32_matmul_precision('high')
+
     model = Classifier(
         model=instantiate(cfg.model),
-        lr=lr
+        lr=cfg.optim.lr
     )
 
     data = instantiate(
-        cfg.data,
-        batch_size = trial.suggest_categorical('batch_size', [32, 64, 128])
+        cfg.data
     )
     data.prepare_data()
     data.setup("fit")
     train_dataloader = data.train_dataloader()
     val_dataloader = data.val_dataloader()
 
+    callbacks = build_callbacks(cfg)
+
     trainer = Trainer(
         enable_model_summary=False,
         max_epochs=cfg.trainer.epochs,
         accelerator='gpu',
-        devices=[2],
-        callbacks=[
-            RichProgressBar(leave=False),
-            PyTorchLightningPruningCallback(trial, monitor='val_loss'),
-            EarlyStopping(
-                'val_loss',
-                patience=2
-            )
-        ]
+        devices="1",
+        callbacks=callbacks
     )
 
     trainer.fit(model, train_dataloader, val_dataloader)
@@ -56,6 +64,9 @@ def objective(trial: Trial, cfg: DictConfig):
 
 @hydra.main(version_base=None, config_path="pkg://config", config_name="trainer")
 def main(cfg: DictConfig):
+    seed_everything(42, workers=True)
+
+    torch.set_float32_matmul_precision('high')
     study_name = "MNIST"
     storage_name = "sqlite:///{}.db".format(study_name)
     study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True)
