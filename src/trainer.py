c744bcbc4c71d9b638370cc82fb1fbd0f16fe1aa
Christian Mancini <mancinichristian00@gmail.com>
2025-12-30 12:49:49 +0100

    Added configurations

--- src/trainer.py
+++ src/trainer.py
@@ -88,3 +88,37 @@ def hpo(cfg: DictConfig):
         f"{study.best_value}\n"
         f"{study.best_trial.params}\n"
     )
+
+@hydra.main(version_base=None, config_path="pkg://config", config_name="trainer")
+def search(cfg: DictConfig):
+    warnings.filterwarnings("ignore", category=UserWarning, module="lightning")
+
+    seed_everything(cfg.seed, workers=True)
+
+    torch.set_float32_matmul_precision(cfg.precision)
+
+
+
+    model = Classifier(model=instantiate(cfg.model), optimizer=instantiate(cfg.optim))
+
+
+    data = instantiate(cfg.data)
+    data.prepare_data()
+    data.setup("fit")
+    train_dataloader = data.train_dataloader()
+    val_dataloader = data.val_dataloader()
+
+    callbacks = build_callbacks(cfg)
+
+
+    trainer = Trainer(
+        enable_model_summary=False,
+        max_epochs=cfg.trainer.epochs,
+        accelerator=cfg.trainer.accelerator,
+        devices=cfg.trainer.devices,
+        callbacks=callbacks,
+    )
+
+    trainer.fit(model, train_dataloader, val_dataloader)
+
+    return trainer.callback_metrics[cfg.loss.monitor].item()
